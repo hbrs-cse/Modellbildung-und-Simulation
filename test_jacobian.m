e643eb8b1458fbbef12eefd35d547850631b2dab
Jan Kleinert <jan.kleinert@dlr.de>
2019-05-20 12:22:39 +0200

    modify jacobian test

--- test_jacobian.m
+++ test_jacobian.m
@@ -19,18 +19,22 @@ function test_jacobian_of_const
 % test if Jacobian of contant function is zero
     n = randi(10);
     m = randi(10);
-    f = rand()*1e3;
+    c = rand(m,1)*1e3;
+    f = @(x) c;
     x = rand(n,1)*1e3;
-    J = jacobian(@(x) f*ones(m,1),x);
-    assertElementsAlmostEqual(J, zeros(m,n));
+    assertElementsAlmostEqual(jacobian(f,x), zeros(m,n));
 end
 
-function test_x
-% test if jacobian(x) = eye
-    n = randi(10);
-    f = @(x) x;
+function test_linear
+% test if jacobian(Ax+b) = A
+    n = randi(50);
+    A = rand(n,n)*2e2 - 1e2;
+    b = rand(n,1)*2e2 - 1e2;
+    
+    f = @(x) A*x+b;
     x = rand(n,1)*1e3;
-    assertElementsAlmostEqual(jacobian(f,x), eye(n));
+    
+    assertElementsAlmostEqual(jacobian(f,x), A,'relative', 1e-3);
 end
 
 function test_exp
@@ -53,14 +57,14 @@ end
 function test_sin
 % test if jacobian(sin(x)) = cos(x);
     x = rand()*2*pi;
-    assertElementsAlmostEqual(jacobian(@sin,x),cos(x),'relative',1e-5);
+    assertElementsAlmostEqual(jacobian(@sin,x),cos(x),'relative',1e-3);
 end
 
 
 function test_cos
 % test if jacobian(cos(x)) = -sin(x);
     x = rand()*2*pi;
-    assertElementsAlmostEqual(jacobian(@cos,x),-sin(x),'relative',1e-5);
+    assertElementsAlmostEqual(jacobian(@cos,x),-sin(x),'relative',1e-3);
 end
 
 function test_monomial
