9ebcc271350d8779358f4a4244b226ca22c4c65b
MartinKliemank <martin.kliemank@gmx.de>
2019-10-23 14:01:11 +0200

    took changes from other spline interpolation branch and copied onto master

---
+++ _build/05_interp_approx/quadraticSpline.m
@@ -0,0 +1,7 @@
+function zfine = quadraticSpline(x, z, xfine, bc)
+% z = quadraticSpline(x, z, xfine, bc) calculates the quadratic spline going through the
+% points defined by xi and zi and evaluates the spline at the points in xfine.
+
+...
+
+end
