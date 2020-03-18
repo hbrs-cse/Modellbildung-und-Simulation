fd9e8cb1a46867fa1b79eb9eb2523c24a6f4f8fe
MartinKliemank <martin.kliemank@gmx.de>
2020-03-18 19:57:52 +0100

    (hopefully) updated .circleci/config.yml and .gitignore with files from master

--- _build/05_interp_approx/quadraticSpline.m
+++
@@ -1,7 +0,0 @@
-function zfine = quadraticSpline(x, z, xfine, bc)
-% z = quadraticSpline(x, z, xfine, bc) calculates the quadratic spline going through the
-% points defined by xi and zi and evaluates the spline at the points in xfine.
-
-...
-
-end
