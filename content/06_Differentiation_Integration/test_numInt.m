d989d8db58e36818fdb643480638b850091d6bff
MartinKliemank <martin.kliemank@gmx.de>
2020-03-22 00:45:10 +0100

    copied changes from diff_int_exercise branch to this one (i think that would be called rebasing)

---
+++ content/06_Differentiation_Integration/test_numInt.m
@@ -0,0 +1,56 @@
+function test_suite=test_quadraticSpline
+% initialize unit tets
+    try
+        test_functions=localfunctions();
+    catch
+    end
+    initTestSuite;
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%     Basic tests     %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+function test_numInt_basic
+% test function of basic function
+    a = 0;
+    b = 1;
+    f = @(x) x;
+    tol = 10^-6;
+    assertVectorsAlmostEqual(numInt(f,a,b,tol),0.5)
+    
+function test_numInt_negativeStartOfInterval
+% test function of negative start of interval
+    a = -1;
+    b = 1;
+    f = @(x) x;
+    tol = 10^-6;
+    assertVectorsAlmostEqual(numInt(f,a,b,tol),0)
+    
+function test_numInt_moreComplexFunction
+% test function of 
+    a = -1;
+    b = 1;
+    f = @(x) x^3+x;
+    tol = 10^-6;
+    assertVectorsAlmostEqual(numInt(f,a,b,tol),0) 
+    
+%%%%%%%%%%%%%%%%%%%%%%%
+% More advanced tests %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+function test_numInt_invertedInterval
+% test function of basic function
+    a = 1;
+    b = 0;
+    f = @(x) x;
+    tol = 10^-6;
+    assertVectorsAlmostEqual(numInt(f,a,b,tol),-0.5)
+    
+function test_numInt_poleInIntervall
+% test function of 
+    a = -1;
+    b = 1;
+    f = @(x) 1/x;
+    tol = 10^-6;
+    numInt(f,a,b,tol)
+    assertExceptionThrown(numInt(f,a,b,tol))  
