304dea6b2e80bf5c5599c17a61943978fbaeefba
Jan Kleinert <jan.kleinert@dlr.de>
2020-04-21 15:51:17 +0200

    add fourier approx exercise

---
+++ content/09_dft/fourier_approx_rectangular.m
@@ -0,0 +1,9 @@
+function a = fourier_approx_rectangular(amplitude, freq, f_max)
+% calculates the complex Fourier coefficients of a periodic rectangular signal.
+%
+% amplitude denotes the amplitude of the input signal [unit implied]
+% freq denotes the frequency of the input signal [Hz]
+% f_max denotes the highest frequency to be used for the approximation [Hz]
+% 
+%  INSERT CODE HERE
+%
